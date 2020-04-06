50ba8e0ca8c6d41e4dfc023c1114a309196eb029
unknown <pablo@DXSPR003.dixis.es>
2020-04-06 03:12:31 +0200

    primer commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// REQUIRES
+var express = require('express');
+var mongoose = require('mongoose');
+
+
+
+//Inicializar variables
+
+var app = express();
+var verde = '\x1b[32m%s\x1b[0m';
+
+
+//Conexion a la base de datos
+
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+
+    if (err) throw err;
+    console.log('Base de datos' + verde, ' online');
+
+
+})
+
+//Rutas
+
+app.get('/', (req, res, next) => {
+
+    res.status(200).json({
+        ok: true,
+        mensaje: 'Peticion realizada correctamente'
+    });
+
+})
+
+//Escuchar peticiones
+app.listen(3000, () => {
+    console.log('Express ok on port: ' + verde, '3000');
+
+});
